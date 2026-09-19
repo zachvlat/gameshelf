7472adb7efad689db6b3d15b0cba46d78a55a246
zachvlat <zachariasvlatakis@gmail.com>
2026-09-19 13:26:27 +0300

    version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.zachvlat.gamelibrary"
         minSdk = 26
         targetSdk = 36
-        versionCode = 21
+        versionCode = 22
         versionName = "4.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
